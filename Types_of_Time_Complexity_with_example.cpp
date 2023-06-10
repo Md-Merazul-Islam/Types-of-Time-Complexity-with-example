a38408e305b6e05110e6077953ef2941dc55fc38
Md-Merazul-Islam <mdmerazul75@gmail.com>
2023-06-10 12:18:21 +0600

    first commit

---
+++ Types_of_Time_Complexity_with_example.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    //O(1)
+    result = a+b
+    //O(n)
+    for(int i=0; i<n; i++)
+    {
+        //statement;
+    }
+    //O(n^2)
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+ 
+        }
+    }
+ 
+    for(int i=n/2; i<n; i++)
+    {
+        for(int j=1; j<n; j++)
+        {
+ 
+        }
+    }
+ 
+    //O(n^3)
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            for(int k = 0; k < n; k++)
+            {
+                // some constant time operation
+            }
+        }
+    }
+ 
+    //O(logn)
+    for(int i=1; i<n; i = i* 2)
+    {
+        //statement
+    }
+ 
+    //O(nlogn)
+    for(int i=n/2; i<n; i++)
+    {
+        for(int j=1; j<n; j=j*2)
+        {
+            //statement
+        }
+    }
+ 
+    //O(sqrt(n)
+ 
+    for (int i = 1; i*i <= n; i++)
+    {
+        // statement
+    }
+ 
+    //O(2^n)
+    for (int i = 0; i < pow(2, n); i++)
+    {
+        // code goes here
+    }
+ 
+ 
+    return 0;
+}
